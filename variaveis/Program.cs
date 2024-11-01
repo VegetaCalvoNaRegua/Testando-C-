3819defdbdd7f99998e903b34d4749b53d547df2
VegetaCalvoNaRegua <vegetasemicalvo@hotmail.com>
2024-11-01 16:33:46 -0400

    tipos de dados nao primitivos

--- variaveis/Program.cs
+++ variaveis/Program.cs
@@ -23,7 +23,64 @@ char inicial = "W";
 
 //tipos logicos
 
-bool estaLogado = true
+bool estaLogado = true;
 
-bool ativo = false'
+bool ativo = false;
+
+//tipos de dados nao primitivos
+
+string saudacao = "Ola Mundo";
+
+
+//Arrays
+int[] numeros ={1,2,3,4,5};
+string[] nomes = {"Wanderson", "Ryas", "Jeane"};
+
+
+//Classe
+
+plubic class Ponto   
+
+{
+public class Pessoa;
+
+public stream Nome;
+
+public string idade;
+}
+
+
+//Estruct
+
+plubic class Ponto   
+
+{
+public int X;
+
+public int Y;
+
+}
+
+//Enums: sao usadoas para definir conjutos constantes
+
+public enum DiadaSemana
+{
+    Segunda, Terca, Quarta, QUinta, Sexta, Sabado, Domingo
+}
+
+//Interface
+
+public interface Veiculo
+{
+    void Mover();
+}
+
+//Delegates
+
+public void delegates Processar (int numero);
+
+
+//NUllabes types
+
+int? idade = null;
 
