bdbf77123c3d3a013dab869fde30acd7101ea6f2
VegetaCalvoNaRegua <vegetasemicalvo@hotmail.com>
2024-11-01 16:07:49 -0400

    tipos de dados primitivos

--- variaveis/Program.cs
+++ variaveis/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//tipos de dados primitivos
+//numeros inteiros
+
+short ano = 2025
+
+int contador = 10
+
+long populacaoMundial 78000000000;
+
+
+// numeros de ponto flutante
+
+float temperatiraa = 43,6F;
+
+double precoProdtuo = 199.90;
+
+//Tipo caracter
+
+char inicial = "W";
+
+//tipos logicos
+
+bool estaLogado = true
+
+bool ativo = false'
+
