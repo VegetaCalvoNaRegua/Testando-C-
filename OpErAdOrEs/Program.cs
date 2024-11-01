5385082f4b11684f1a6d142fec020467967d6b66
VegetaCalvoNaRegua <vegetasemicalvo@hotmail.com>
2024-11-01 17:21:29 -0400

    Operadores finalizados

--- OpErAdOrEs/Program.cs
+++ OpErAdOrEs/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+double resultado = 12 + 8 + 12 /3;
+Console.WriteLine(resultado);
+//// eifjeiwfj
+double resultado2 = (12 + 8 + 12) /3;
+Console.WriteLine(resultado2);
+////
+double resultado3 = 2*2;
+Console.WriteLine(resultado3);
