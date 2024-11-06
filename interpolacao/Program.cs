09b326ffd215be3fb7297499dc78d56ab601c5d2
EmmanuelPlatinum <141779528+EmmanuelPlatinum@users.noreply.github.com>
2024-11-06 16:00:32 -0400

    exemplos de casting

--- interpolacao/Program.cs
+++ interpolacao/Program.cs
@@ -30,3 +30,51 @@ Console.WriteLine("A variavel valorTexto eh do tipo:" + valorTexto.GetType());
 int numero22 = int.Parse(valorTexto);
 Console.WriteLine(numero22);
 Console.WriteLine ("A variavel do Parse eh do tipo:" +numero22.GetType());
+
+
+// metodo Convert
+string valorEmTexto = "100";
+
+int numeroConvertido = Convert.ToInt32(valorEmTexto);
+Console.WriteLine(numeroConvertido.GetType());
+
+
+// metodo To String, usado para converter qualquer tipo em string
+
+int numero = 123;
+Console.WriteLine(numero.GetType());
+
+string numeroComoString = numero.ToString();
+Console.WriteLine(numeroComoString.GetType());
+
+// Personalizacao de Formatacao Data e Hora
+
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine(dataAtual);
+
+string dataFormatada = dataAtual.ToString("dd/MM/yyyy");
+Console.WriteLine(dataFormatada);
+
+string dataHoraFormat = dataAtual.ToString("hh:mm:ss");
+Console.WriteLine(dataHoraFormat);
+
+
+// To String com $
+
+double preco = 29.99;
+Console.WriteLine($"O preco eh: {preco.ToString("C2")}");
+
+ 
+// Culturalizacao de variaveis
+
+decimal valor = 1234.56m;
+string valorUS = valor.ToString("C", new System.Globalization.CultureInfo ("en-US"));
+Console.WriteLine(valorUS);
+
+string valorBR = valor.ToString("C", new System.Globalization.CultureInfo ("pt-BR"));
+Console.WriteLine(valorBR);
+
+
+//
+
+
