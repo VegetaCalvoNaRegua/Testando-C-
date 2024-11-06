6ef97e00ff49ac3748e5c4aa6d261d33308f648e
EmmanuelPlatinum <141779528+EmmanuelPlatinum@users.noreply.github.com>
2024-11-06 16:14:44 -0400

    condicionais

---
+++ Condicionais/Program.cs
@@ -0,0 +1,15 @@
+﻿double nota1 = 6.5;
+double nota2 = 5.9;
+double nota3 = 8.1;
+
+double media = (nota1 + nota2 + nota3) / 3;
+if (media >= 7)
+{
+  Console.WriteLine($"Media {media:F2}; aluno aprovado!");
+}
+else
+{
+  Console.WriteLine($"Media {media:F2}; aluno em recuperacao!");
+}
+
+
