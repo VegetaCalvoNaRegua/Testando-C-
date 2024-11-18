4c00f10fb809f91c1e499e78d70bc45fd5267894
Vegeta Príncipe Calvo <vegetasemicalvo@hotmail.com>
2024-11-18 17:12:54 -0400

    Exercicios Estrutura de Dados

--- Exercicios_Estrutura/Program.cs
+++ Exercicios_Estrutura/Program.cs
@@ -1,4 +1,4 @@
-﻿// escreva um codigo que some todos os elementos do array
+﻿// Exercicio 1: escreva um codigo que some todos os elementos do array
 
 using System.Net.Http.Headers;
 using System.Runtime.CompilerServices;
@@ -12,7 +12,7 @@ for (int i = 0; i < Array.Length; i++){
 Console.WriteLine(somaArray);
 
 
-//Exercicio resolvido pelo professor
+//Exercicio 1 resolvido pelo professor
 
 // int[] numeros = {1, 2, 3, 4, 5};
 // int soma = 0;
@@ -20,9 +20,18 @@ Console.WriteLine(somaArray);
 //     soma += numero;
 // }
 
-// // escreva um codigo para contar quantas vezes determinada string aparece listada
+//Exercicio 2: escreva um codigo para contar quantas vezes determinada string aparece listada
 
-// List<string> nome  = new List<string> {"Jeane", "Anderson", "Jeane", "Ryan", "Jeane"};
+ List<string> nome  = new List<string> {"Jeane", "Anderson", "Jeane", "Ryan", "Jeane"};
+ int contador = 0;
+ foreach (string pessoa in nome){
+
+    if  (pessoa == "Jeane"){
+        
+        contador ++;
+    }
+ }
+ Console.WriteLine($"Numero de vezes que o nome se repete:{contador}");
 
 
 
@@ -39,6 +48,12 @@ foreach (int numero in conjunto){
     Console.WriteLine(numero);
 }
 
+//Exercicio 4
+
+HashSet<string> frutas = new HashSet<string> {"Maçã", "Banana", "Laranja"};
+Console.WriteLine("Banana está no conjunto frutas?");
+Console.WriteLine(frutas.Contains("Banana") ? "Sim, Banana está no conjunto frutas" : "Não, Banana não está no conjunto frutas");
+
 
 
 //Exercicio 5
@@ -50,13 +65,6 @@ List<int> listadenumerosvazia = new List<int>();
  }
 
 Console.WriteLine("Elementos na lista:");
-foreach (int )
-
-
-
-
-
-
-
-
-
+for (int i = 0; i < listadenumerosvazia.Count; i++ ){
+    Console.WriteLine(listadenumerosvazia[i]);
+}
