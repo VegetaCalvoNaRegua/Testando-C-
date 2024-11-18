d7600f81a22e7141ba2f8a3ba0a0db4adfa42430
Vegeta Príncipe Calvo <vegetasemicalvo@hotmail.com>
2024-11-18 16:52:50 -0400

    Estrutura de Dados Exercicios

---
+++ Estrutura_de_Dados/Program.cs
@@ -0,0 +1,50 @@
+﻿///
+///
+///
+///
+///
+///
+///
+///
+///
+
+
+//Exemplos 03: Conjunto (Set)
+//Conjunto Set os elementos dispostos nao se repetem, nao permite insercao de elementos repetidos.
+
+HashSet<int> conjunto = new HashSet<int> {1,2,3};
+conjunto.Add(4);
+conjunto.Add(2);
+
+Console.WriteLine("Elementos do Conjunto");
+foreach(int elemento in conjunto){
+
+    Console.WriteLine(elemento);
+}
+
+
+// Exemplo 3.1
+
+HashSet<string> frutas = new HashSet<string> {"Maca", "Abacaxi", "Laranja"};
+frutas.Add("Uva");
+frutas.Add("Maca");
+
+Console.WriteLine("Frutas no conjunto:");
+foreach (string fruta in frutas)
+{
+    Console.WriteLine (fruta);
+}
+
+Console.WriteLine("\nVerificacao de Existencia:");
+Console.WriteLine(frutas.Contains("Banana") ? "Banana estah no conjunto" : "Banana nao estah no conjunto");
+
+
+//Exemplo 3.2: Remocao de elementos
+
+frutas.Remove("Laranja");
+Console.WriteLine ("\nApos remover Laranja:");
+foreach (string fruta in frutas){
+    Console.WriteLine(fruta);
+}
+
+//Exemplo 3.3
---
+++ Exercicios_Estrutura/Program.cs
@@ -0,0 +1,62 @@
+﻿// escreva um codigo que some todos os elementos do array
+
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+
+int [] Array = new int[5] {2, 4, 6, 8, 10};
+int somaArray = 0;
+for (int i = 0; i < Array.Length; i++){
+    somaArray += Array[i];
+}
+
+Console.WriteLine(somaArray);
+
+
+//Exercicio resolvido pelo professor
+
+// int[] numeros = {1, 2, 3, 4, 5};
+// int soma = 0;
+// foreach ( int numero in numeros){
+//     soma += numero;
+// }
+
+// // escreva um codigo para contar quantas vezes determinada string aparece listada
+
+// List<string> nome  = new List<string> {"Jeane", "Anderson", "Jeane", "Ryan", "Jeane"};
+
+
+
+
+
+// Exercicio 3
+
+List<int> listadenumeros = new List<int> {1,2,3,4,2,3,1,2,4};
+HashSet<int> conjunto = new HashSet<int>(listadenumeros);
+
+Console.WriteLine("Lista sem duplicatas:");
+foreach (int numero in conjunto){
+
+    Console.WriteLine(numero);
+}
+
+
+
+//Exercicio 5
+
+List<int> listadenumerosvazia = new List<int>();
+ for (int i = 1; i <=5; i++){
+
+    listadenumerosvazia.Add(i);
+ }
+
+Console.WriteLine("Elementos na lista:");
+foreach (int )
+
+
+
+
+
+
+
+
+
