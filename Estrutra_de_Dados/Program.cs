25c31fb5b6fd1a3920d78bffad49adeb91d4cba1
Vegeta Príncipe Calvo <vegetasemicalvo@hotmail.com>
2024-11-18 14:10:29 -0400

    estrutura de dados

---
+++ Estrutra_de_Dados/Program.cs
@@ -0,0 +1,23 @@
+﻿///
+///
+///
+///
+///
+///
+///
+///
+///
+
+
+//Exemplos 03: Conjunto (Set)
+//Conjunto Set os elementos dispostos nao se repetem, nao permite insercao de elementos repetidos.
+
+HashSet<int> conjunto = new HashSet<int> {1,2,3};
+conjunto.Add(4);
+conjunto.Add(2);
+
+Console.WriteLine("Elementos do Conjunto");
+foreach(int elemento in conjunto){
+
+    Console.WriteLine(elemento);
+}
---
+++ repeticao/Program.cs
@@ -0,0 +1,6 @@
+﻿//Estruturas de Repeticao 
+//Sao ferramentas que permitem a automatizacao repetida de um bloco de dados 
+// necessario adicionar uma condicao para que o processo termine devido ao cumprimento de uma condicao especifica
+
+
+// while eh o comando utilizado para 
