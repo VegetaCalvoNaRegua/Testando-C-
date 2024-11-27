8dbac4a4d5c9ef17a4a6242ef61e6caa498a115d
Vegeta Príncipe Calvo <vegetasemicalvo@hotmail.com>
2024-11-27 12:00:32 -0400

    aaaa

---
+++ Classes_solo/Mensangens.cs
@@ -0,0 +1,24 @@
+﻿using Sytem;
+
+namespace Programa
+{
+  static class Mensangens
+ {
+    static void Oi()
+    {
+      Console.WriteLine("Oi! Bem vindo ao programa.");  
+    }
+
+    static void Esperando()
+    {
+        Console.WriteLine("Estou esperando por algo...");
+    }
+
+    static void Tchau()
+    {
+        Console WriteLine("Até mais! Obrigado por vir.");
+    }
+ }
+
+}
+
---
+++ Classes_solo/Programinha.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Programa
+{
+    
+     class Programinha
+     {
+
+          static void Main(string[] args)
+          {
+
+            Mensagens.Oi(); 
+            Mensagens.Esperando();
+            Mensangens.Tchau();
+
+            Console.ReadKey();
+
+          }
+
+     }
+
+}
