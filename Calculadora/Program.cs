87e2823675b244943e3feeab1ee9887d8f590e94
EmmanuelPlatinum <141779528+EmmanuelPlatinum@users.noreply.github.com>
2024-11-27 17:14:40 -0400

    projeto calculadora

---
+++ Calculadora/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Resultado
+{
+    internal class Programa
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual operacao deseja fazer?");
+            Console.WriteLine("1 - Adicao");
+            Console.WriteLine("2 - Subtracao");
+            Console.WriteLine("3 - Multiplicacao");
+            Console.WriteLine("4 - Divisao");
+
+            Console.ReadLine();
+
+            int operacao = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o primeiro numero para a operacao a ser realizada:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo numero para a operacao a ser realizada:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int resultado = 0;
+
+            switch (operacao)
+            {
+                case 1:
+                {
+                    resultado = Adicao(num1, num2);
+                    break;
+                }
+
+                case 2:
+                {
+                    resultado = Subtracao(num1, num2);
+                    break;
+                }
+
+                case 3:
+                {
+                    resultado = Multiplicacao(num1, num2);
+                    break;
+                }
+
+                case 4:
+                {
+                    resultado = Divisao(num1, num2);
+                    break;
+                }
+
+                default:
+                Console.WriteLine("Numero invalido, digite outro valor:");
+                break;
+            }
+          
+          Console.WriteLine($"O resultado da operacao eh:{resultado}");
+
+        } 
+    
+    public static int Adicao(int num1, int num2)
+     {
+        int resultado = num1 + num2;
+        return resultado;    
+
+     }
+
+     public static int Subtracao(int num1, int num2)
+     {
+        int resultado = num1 - num2;
+        return resultado;    
+
+     }
+
+     public static int Multiplicacao(int num1, int num2)
+     {
+        int resultado = num1 * num2;
+        return resultado;    
+
+     }
+
+     public static int Divisao(int num1, int num2)
+     {
+        if (num2 == 0)
+        {
+            Console.WriteLine("Erro, divisao pro 0 nao permitida");
+            return 0;
+        }
+        int resultado = num1 / num2;
+        return resultado; 
+
+
+     }
+    
+    }
+
+
+}
---
+++ POO_Poli/Program.cs
@@ -0,0 +1,18 @@
+﻿// Programacao Orientada a Objetos, pilar "POLIMORFISMO"
+
+// Polimorfismo eh a capacidade de objetos de classes diferentes responderem a mesma chamada de metodo de maneiras distintas
+
+// Podemos aplicar de duas formas diferentes
+
+// Override
+// Ocorre, por exemplo, quando uma subclasse redefine o metodo herdado de sua superclasse 
+// Eh utilizado o modificador 'virtual' para o metodo que pode ser sobrescrito
+
+
+// Overload
+// Capacidade de definir varios metodos com o mesmo nome em uma classe, com diferentes listas de parametros.
+// 
+
+
+// Abstracao
+// Na abstracao os metodos de uma classe abstrata nao existem, sendo necessarios
